9412e6ca107c19d321b39ee5a5e1ad5f85781695
Nuttiphun <nutchanock12@gmail.com>
2024-09-16 08:07:55 +0700

    Implement HomePage.vue

---
+++ src/page/Home/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Welcome to JUSTPOST</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({ name: 'HomePage' })
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+  text-align: center;
+}
+</style>
